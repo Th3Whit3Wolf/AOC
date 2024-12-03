690bc22a5dd33b17e4977297d2aba798062be578
David Karrick <48275422+Th3Whit3Wolf@users.noreply.github.com>
2024-12-03 16:05:56 -0700

    Feat: Complete AOC 2024 day 1 part1

--- 2024/rust/day-01/src/part1.rs
+++ 2024/rust/day-01/src/part1.rs
@@ -1,20 +1,41 @@
-use aoc::AocError;
-
 #[tracing::instrument]
-pub fn process(input: &str) -> miette::Result<String, AocError> {
-    todo!("aoc_2024_1 - part 2");
+pub fn process(input: &str) -> miette::Result<String> {
+    let mut left: Vec<i32> = Vec::with_capacity(1000);
+    let mut right: Vec<i32> = Vec::with_capacity(1000);
+
+    for l in input.lines() {
+        let s = l.split_once("   ").unwrap();
+        left.push(s.0.parse::<i32>().unwrap());
+        right.push(s.1.parse::<i32>().unwrap());
+    }
+
+    left.sort();
+    right.sort();
+
+    assert_eq!(left.len(), right.len());
+
+    let mut total = 0;
+    for i in 0..left.len() {
+        let value = (left[i] - right[i]).abs();
+        total += value
+    }
+
+    Ok(total.to_string())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use pretty_assertions::assert_eq;
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+        assert_eq!("11", process(input)?);
         Ok(())
     }
 }
