3e50b993ac100ed5554b438b22739b369b44d9bc
David Karrick <48275422+Th3Whit3Wolf@users.noreply.github.com>
2024-12-03 16:06:37 -0700

    Feat: Complete AOC 2024 day 1 part 2

--- 2024/rust/day-01/src/part2.rs
+++ 2024/rust/day-01/src/part2.rs
@@ -1,20 +1,51 @@
-use aoc::AocError;
+use std::collections::HashMap;
 
 #[tracing::instrument]
-pub fn process(input: &str) -> miette::Result<String, AocError> {
-    todo!("aoc_2024_1 - part 2");
+pub fn process(input: &str) -> miette::Result<String> {
+    let mut map: HashMap<u32,u32> = HashMap::with_capacity(1000);
+    let mut left: Vec<u32> = Vec::with_capacity(1000);
+    let mut right: u32 = 0;
+
+    for l in input.lines() {
+        let s = l.split_once("   ").unwrap();
+        right = s.1.parse::<u32>().unwrap();
+        left.push(s.0.parse::<u32>().unwrap());
+
+        if let Some(v) = map.get_mut(&right) {
+            *v += 1;
+        } else {
+            map.insert(right, 1);
+        }
+    }
+
+
+    let mut total = 0;
+
+    for num in left {
+        if let Some(v) = map.get(&num) {
+            total += v*num
+        }
+    }
+
+
+    println!("map: {:#?}", map);
+
+    Ok(total.to_string())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use pretty_assertions::assert_eq;
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+        assert_eq!("31", process(input)?);
         Ok(())
     }
 }
